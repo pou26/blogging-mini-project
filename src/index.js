4402707b0693e2420f797411e88215710a63c5c9
pou26 <poushaliaich1999@gmail.com>
2022-11-22 12:53:29 +0530

    final

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./route/route.js');
-const {default: mongoose}  = require('mongoose');
+const mongoose  = require('mongoose');
 
 const app = express();
 
